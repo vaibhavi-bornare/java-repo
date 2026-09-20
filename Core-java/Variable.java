ff96a625045cc4508e9df13d312a3eb8f77150ab
vaibhavi-bornare <bornarevaibhavi269@gmail.com>
2026-09-20 21:26:48 +0530

    creating variable

---
+++ Core-java/Variable.java
@@ -0,0 +1,11 @@
+public class Variable {
+    public static void main(String[] args) {
+        int num=10;
+       char grade = 'A';
+       String name = "Vaibhavi";
+       System.out.println(num);
+       System.out.println(grade);
+       System.out.println(name);
+    
+    }
+}
