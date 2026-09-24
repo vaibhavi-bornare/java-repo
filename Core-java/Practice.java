811ba8b2a637480c3206b89bde501de6265a67c6
vaibhavi-bornare <bornarevaibhavi269@gmail.com>
2026-09-24 19:16:55 +0530

    practice question

---
+++ Core-java/Practice.java
@@ -0,0 +1,18 @@
+public class Practice {
+    public static void main(String[] args) {
+        int StudentId=1;
+        int StudentAge=21;
+        int JavaMarks=82;
+        int SqlMarks=90;
+        int HtmlMarks=87;
+        int totalmarks=JavaMarks+SqlMarks+HtmlMarks;
+        int avgmarks=totalmarks/3;
+        System.out.println("StudentId : "+ StudentId);
+        System.out.println("StudentAge : "+ StudentAge);
+        System.out.println("JavaMarks : "+ JavaMarks);
+        System.out.println("SqlMarks : "+  SqlMarks);
+        System.out.println(" HtmlMarks: "+ HtmlMarks);
+         System.out.println("totalmarks : "+ totalmarks);
+          System.out.println("avgmarks : "+ avgmarks);
+    }
+}
